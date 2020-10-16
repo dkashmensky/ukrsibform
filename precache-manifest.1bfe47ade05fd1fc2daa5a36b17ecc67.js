1ca6d7ae187f4a53af2c812868a3cfe77b08d240
Dmitry Kashmenskiy <dkashmenskiy@IV.LOCAL>
2020-10-16 12:19:04 +0300

    Updates

---
+++ precache-manifest.1bfe47ade05fd1fc2daa5a36b17ecc67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2d3835ab1d1b5654c4e4177aee452c9",
+    "url": "/ukrsibform/index.html"
+  },
+  {
+    "revision": "69a607fedfe7dfb2dc54",
+    "url": "/ukrsibform/static/css/main.659d6668.chunk.css"
+  },
+  {
+    "revision": "036a6d5f4e8f2ec546bc",
+    "url": "/ukrsibform/static/js/2.7af42c19.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/ukrsibform/static/js/2.7af42c19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69a607fedfe7dfb2dc54",
+    "url": "/ukrsibform/static/js/main.ee613ce9.chunk.js"
+  },
+  {
+    "revision": "144cc6973d304258aa49",
+    "url": "/ukrsibform/static/js/runtime-main.d6a0d30f.js"
+  },
+  {
+    "revision": "22e7c5056106d9b56c73b501471c1e8d",
+    "url": "/ukrsibform/static/media/logo.22e7c505.svg"
+  }
+]);
